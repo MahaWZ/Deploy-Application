ad2823d304a888e69c6b971362b7e753f72dab81
Ubuntu <ubuntu@ip-172-31-20-110.ap-southeast-2.compute.internal>
2023-11-28 11:05:44 +0000

    automate task1 done

---
+++ task1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir TechKaro
+cd TeckKaro
+touch task1.py
+echo "This is my task 1 of +W">task1.py
+USERNAME="MahaWZ"
+TOKEN="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"
+git add .
+git commit -m "automate task1 done"
+git push origin main
+$USERNAME
+$TOKEN
